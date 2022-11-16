22c6100f2e2e633920ab0e19613763faf6c10dc7
Tigioz <z.fouts@icloud.com>
2022-11-16 11:58:15 -0600

    initial

---
+++ src/main/java/com/example/codingevents/controllers/EventController.java
@@ -0,0 +1,39 @@
+package com.example.codingevents.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    private static List<String> events = new ArrayList<>();
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+
+
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    //  at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+    //  at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
+    }
+
+
+}
