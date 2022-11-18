225dc34623b6f6ca828f08a4f6b0f8cefdf109b0
Tigioz <z.fouts@icloud.com>
2022-11-17 20:51:51 -0600

    chpt 14 exercises complete

--- src/main/java/com/example/codingevents/controllers/EventController.java
+++ src/main/java/com/example/codingevents/controllers/EventController.java
@@ -1,6 +1,7 @@
 package com.example.codingevents.controllers;
 
 
+import com.example.codingevents.data.EventData;
 import com.example.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,17 +16,14 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    public static HashMap<Event, String> events = new HashMap();
 
 
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        events.put(new Event("Menteaship"),"A fun meetup for connecting with mentors");
-        events.put(new Event("Code With Pride"),"A fun meetup sponsored by LaunchCode");
-        events.put(new Event("Javascripty"), "An imaginary meetup for Javascript developers");
 
-        model.addAttribute("events", events);
+
+        model.addAttribute("events", EventData.getAll());
         return "/events/index";
     }
 
@@ -36,10 +34,29 @@ public class EventController {
     }
     //  at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
-        events.put(new Event(eventName), eventDescription);
+    public String createEvent(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
         return "redirect:";
     }
 
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+
+        return "/events/delete";
+
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds) {
+
+        if (eventIds != null) {
+        for (int id: eventIds) {
+            EventData.remove(id);
+        }}
+        return "redirect:";
+    }
 
 }
